ab8178e1b1020d352748b36bd7c7cc2b3e88164a
leinue <597055914@qq.com>
2017-05-04 18:08:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ gulp build
 mkdir ./build/weapp/
 cd weapp && npm run build && cd ../
 cp -r ./weapp/dist build/weapp/dist
-mv build/weapp/dist/static ./build
+cp -r build/weapp/dist/static ./build
 git add .
 git commit -a -m 'update'
 git push -u origin master
