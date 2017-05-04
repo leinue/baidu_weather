84d0b6249a7128c6983b948fe318aeb5460641e6
leinue <597055914@qq.com>
2017-05-04 18:10:45 +0800

    updat

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ mkdir ./build/weapp/
 cd weapp && npm run build && cd ../
 cp -r ./weapp/dist build/weapp/dist
 cp -r build/weapp/dist/static ./build
-git add .
-git commit -a -m 'update'
-git push -u origin master
+#git add .
+#git commit -a -m 'update'
+#git push -u origin master
